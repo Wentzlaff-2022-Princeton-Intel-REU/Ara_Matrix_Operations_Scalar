473ac41c0ca9ced47c8ccab6bf0f43857f682568
Manya Zhu <manya.zhu@gmail.com>
2022-07-19 15:10:57 -0400

    add

--- add.c
+++ add.c
@@ -14,20 +14,25 @@ Matrix_t add(Matrix_t mat1, Matrix_t mat2) {
 
     int row = mat1.m;
     int column = mat1.n;
+    printf("1\n");
+
 
     Matrix_t resultingMatrix;
     resultingMatrix.m = row;
     resultingMatrix.n = column;
     
+    printf("2\n");
 
 
     for (int i = 0; i < row; i++){
         for (int j = 0; j < column; j++){
  
             resultingMatrix.elements[i*column + j] = mat1.elements[i*column + j] + mat2.elements[i*column + j];
-
+            printf("3\n");
         }
     }
+    printf("4\n");
+
 
     return resultingMatrix;
 }
