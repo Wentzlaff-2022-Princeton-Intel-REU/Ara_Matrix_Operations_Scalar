89d3ac6691e40e875910461c1decd4e6ae9643cb
m-ukund <mramakri@outlook.com>
2022-07-22 17:20:57 -0400

    changing the multiply algorithm

--- multiply.c
+++ multiply.c
@@ -19,12 +19,31 @@ void multiply(Matrix_t mat1, Matrix_t mat2, Matrix_t* resultingMatrix) {
     resultingMatrix->n = q;
 
 
+    double vc[q];
+
     for (int i = 0; i < p; i++) {
-        for (int j = 0; j < q; j++) {
-            resultingMatrix->elements[i * q + j] = 0;
+
+        int avl = q;
+
+        for (int j = 0; j < q; j++) { // This is the stripmining - equivalent loop. 
+
+           for (int m = 0; m < q; m++){
+            vc[m] = 0;
+           }
+
+            // resultingMatrix->elements[i * q + j] = 0;
             for (int k = 0; k < n; k++) {
-                resultingMatrix->elements[i * q + j] += mat1.elements[i * q + k] * mat2.elements[k * q + j];
+
+                for (int l = 0; l < q; l++){
+                    vc[l] = vc[l] + (mat1.elements[i*n+k] * mat2.elements[j * q]);
+                    // resultingMatrix->elements[i*q+l] = resultingMatrix->elements[i*q+l] + (mat1.elements[i*n+k] * mat2.elements[j * q + l]);
+              }
+
+                resultingMatrix->elements[i*q] = vc;
+               // resultingMatrix->elements[i * q + j] += mat1.elements[i * q + k] * mat2.elements[k * q + j];
             }
+
+            
         }
     }
 
