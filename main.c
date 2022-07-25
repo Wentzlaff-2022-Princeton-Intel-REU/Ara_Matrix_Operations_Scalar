fc2fa87a409ae100f2acc2e20fe0b49dec20c764
m-ukund <mramakri@outlook.com>
2022-07-25 15:29:30 -0400

    4x4

--- main.c
+++ main.c
@@ -12,12 +12,13 @@
 
 int main() {
     Matrix_t matrix_A, matrix_B;
-    matrix_A.m = 5;
-    matrix_A.n = 5;
-    matrix_B.m = 5;
-    matrix_B.n = 5;
-    int32_t arr[25] = {59, 63, 63, 28, 76, 64, 32, 43, 36, 63, 28, 76, 64, 32, 43, 36, 43, 36, 63, 28, 76, 64, 32, 43, 36};
-    int32_t arr2[25] = {59, 63, 63, 28, 76, 64, 32, 43, 36, 63, 28, 76, 64, 32, 43, 36, 43, 36, 63, 28, 76, 64, 32, 43, 36};
+   matrix_A.m = 4;
+    matrix_A.n = 4;
+    matrix_B.m = 4;
+    matrix_B.n = 4;
+    int32_t arr[16] = {59, 63, 63, 28, 76, 64, 32, 43, 36, 63, 28, 76, 64, 32, 43, 36};
+    int32_t arr2[16] = {59, 63, 63, 28, 76, 64, 32, 43, 36, 63, 28, 76, 64, 32, 43, 36};
+
     matrix_A.elements = arr;
     matrix_B.elements = arr2;
 
@@ -29,7 +30,7 @@ int main() {
    // printf("\n");
 
     Matrix_t sum;
-    int32_t elementsSum[25];
+    int32_t elementsSum[16];
     sum.elements = elementsSum;
 
     start_timer();
@@ -49,7 +50,7 @@ int main() {
   //  printf("\n");
 
     Matrix_t product;
-    int32_t elementsProd[25];
+    int32_t elementsProd[16];
     product.elements = elementsProd;
     
     start_timer();
